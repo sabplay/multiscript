e5f818aa7e3e63143f68489bbb02d7e7dec60e5e
sabplay <87631627+sabplay@users.noreply.github.com>
2023-03-25 15:15:49 +0530

    Any executor

--- FRONTLINES.lua
+++ FRONTLINES.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/FRONTLINES.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/FRONTLINES.lua"))();
